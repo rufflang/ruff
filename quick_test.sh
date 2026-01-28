c09be7df0fd65486447842303516d45137ef9869
Robert DeVore <deviodigital@gmail.com>
2026-01-28 10:46:26 -0500

    :ok_hand: IMPROVE: Cleaned up test files and other junk

--- quick_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Building..."
-cargo build --release 2>&1 > build.log
-echo "Build done. Running benchmark..."
-cd benchmarks/cross-language && ./run_benchmarks.sh
-echo "Check latest results file!"
